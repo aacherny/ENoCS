443f531e412037b92017b4a14a86eef7cd944d95
chernya <alexanderacherny@gmail.com>
2017-09-26 11:51:02 -0400

    Other buttons like 'Exit' added to the 'File' menu

--- src/main/java/OuterJFrame.java
+++ src/main/java/OuterJFrame.java
@@ -3,6 +3,7 @@ package main.java;
 import javax.swing.*;
 
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 
 /**
@@ -10,16 +11,30 @@ import java.awt.event.KeyEvent;
  */
 public class OuterJFrame
 {
+    private JFrame outerFrame = new JFrame();   // Outer window
+
     public OuterJFrame()
     {
-        JFrame outerFrame = new JFrame();   // Outer window
-        JMenuBar menuBar = new JMenuBar();  // Menu bar within the window
-
+        // General things like window title and size
         outerFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         outerFrame.setTitle("ENoCS Simulator");
         outerFrame.setSize(500, 350);
         outerFrame.setLocationRelativeTo(null);
 
+        // Creates the menu bar and adds it to the window
+        JMenuBar menuBar = createMenuBar();
+        outerFrame.setJMenuBar(menuBar);
+
+        // Makes the window visible
+        outerFrame.setVisible(true);
+    }
+
+    /**
+     * Creates the menu bar in the outer window, adding dropdown menus like
+     * 'File' and 'Edit'
+     */
+    public JMenuBar createMenuBar() {
+        JMenuBar menuBar = new JMenuBar();  // Menu bar within the window
 
         //Build the File menu
         JMenu menu = new JMenu("File");
@@ -31,23 +46,34 @@ public class OuterJFrame
         //'New' button under 'File'
         JMenuItem menuItemNew = new JMenuItem("New");
         menuItemNew.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, ActionEvent.CTRL_MASK));
-        menuItemNew.getAccessibleContext().setAccessibleDescription("Creates a new window");
         menu.add(menuItemNew);
 
         //'Open' button under 'File'
         JMenuItem menuItemOpen = new JMenuItem("Open");
         menuItemOpen.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, ActionEvent.CTRL_MASK));
-        menuItemOpen.getAccessibleContext().setAccessibleDescription("Creates a new window");
         menu.add(menuItemOpen);
+
 
         menu.addSeparator();
 
+        //'Save' button under 'File'
+        JMenuItem menuItemSave = new JMenuItem("Save");
+        menuItemSave.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, ActionEvent.CTRL_MASK));
+        menu.add(menuItemSave);
 
+        //'Save As' button under 'File'
+        JMenuItem menuItemSaveAs = new JMenuItem("Save As");
+        menu.add(menuItemSaveAs);
+
+        menu.addSeparator();
+
+        //'Exit' button under 'File'
+        JMenuItem menuItemExit = new JMenuItem("Exit");
+        menu.add(menuItemExit);
 
 
         outerFrame.setJMenuBar(menuBar);
 
-
-        outerFrame.setVisible(true);
+        return menuBar;
     }
 }
