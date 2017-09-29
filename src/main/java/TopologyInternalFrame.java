528f471e88731cef2d6ea8a26ae3a4f02b370ec0
chernya <alexanderacherny@gmail.com>
2017-09-29 14:11:49 -0400

    Pressing OK in Properties creates a window with the amount of nodes in a mesh configuration

--- src/main/java/TopologyInternalFrame.java
+++ src/main/java/TopologyInternalFrame.java
@@ -1,18 +1,18 @@
 package main.java;
 
-import com.sun.xml.internal.bind.v2.TODO;
+//import com.sun.xml.internal.bind.v2.TODO;
 
 import javax.swing.*;
 
 public class TopologyInternalFrame extends JInternalFrame
 {
     static int openFrameCount = 0;
-    static final int xOffset = 30, yOffset = 30;
+    static final int xOffset = 25, yOffset = 25;
 
-    public TopologyInternalFrame()
+    public TopologyInternalFrame(Network inputNetwork)
     {
-        super("Network Topology" + (++openFrameCount),
-                true, //resizable
+        super("Network Topology",
+                false, //resizable
                 true, //closable
                 true, //maximizable
                 true);//iconifiable
@@ -20,14 +20,33 @@ public class TopologyInternalFrame extends JInternalFrame
         setSize(500, 350);
 
         //Set the window's location.
-        setLocation(xOffset*openFrameCount, yOffset*openFrameCount);
+        setLocation(xOffset, yOffset);
 
         //TODO: Figure out why you can't have both a line and a circle
-        Circle circ1 = new Circle();
-        add(circ1);
+        //Circle circ1 = new Circle();
+        //add(circ1);
 //        Line line1 = new Line();
 //        add(line1);
 
+        if(inputNetwork.getTopology().equals("mesh"))
+        {
+            JPanel panelYContainer = new JPanel();
+            panelYContainer.setLayout(new BoxLayout(panelYContainer, BoxLayout.Y_AXIS));
+            for(int county = 0; county < Math.sqrt(inputNetwork.getNodes()); county++)
+            {
+                JPanel panelXContainer = new JPanel();
+                for(int countx = 0; countx < Math.sqrt(inputNetwork.getNodes()); countx++)
+                {
+                    Circle circle = new Circle();
+                    panelXContainer.add(circle);
+                }
+                panelYContainer.add(panelXContainer);
+            }
+
+            add(panelYContainer);
+        }
+
+        pack();
         setVisible(true);
     }
 }
